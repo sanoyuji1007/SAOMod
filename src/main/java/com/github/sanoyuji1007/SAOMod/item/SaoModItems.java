71c65275ae026b3cab3e7b7b3397f70675a5967e
sanoyuji1007 <sanoyuji1007@gmail.com>
2026-03-25 23:21:51 +0900

    ①レジストリにアイテムを追加

--- src/main/java/com/github/sanoyuji1007/SAOMod/item/SaoModItems.java
+++ src/main/java/com/github/sanoyuji1007/SAOMod/item/SaoModItems.java
@@ -13,6 +13,7 @@ public class SaoModItems {
 
     //  レジストリにアイテムを追加
     public static final RegistryObject<Item> RAW_ORIHALCON = ITEMS.register("raw_orihalcon", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ORIHALCON_INGOT = ITEMS.register("orihalcon_ingot", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
