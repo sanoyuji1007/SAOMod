317a7e5e7b077215fde4b8603524a8d4cef2723b
sanoyuji1007 <sanoyuji1007@gmail.com>
2026-03-25 23:20:30 +0900

    いらないimportが自動で削除

--- src/main/java/com/github/sanoyuji1007/SAOMod/SaoMod.java
+++ src/main/java/com/github/sanoyuji1007/SAOMod/SaoMod.java
@@ -2,10 +2,8 @@ package com.github.sanoyuji1007.SAOMod;
 
 import com.github.sanoyuji1007.SAOMod.item.SaoModItems;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
