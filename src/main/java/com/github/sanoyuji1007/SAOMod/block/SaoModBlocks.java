0f371486d38d8aa730ddb761ea7a36423c63f3f0
sanoyuji1007 <sanoyuji1007@gmail.com>
2026-03-26 23:57:36 +0900

    ORIHALCON_BLOCKを追加

--- src/main/java/com/github/sanoyuji1007/SAOMod/block/SaoModBlocks.java
+++ src/main/java/com/github/sanoyuji1007/SAOMod/block/SaoModBlocks.java
@@ -1,6 +1,9 @@
 package com.github.sanoyuji1007.SAOMod.block;
 
 import com.github.sanoyuji1007.SAOMod.SaoMod;
+import com.github.sanoyuji1007.SAOMod.item.SaoModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
@@ -10,6 +13,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class SaoModBlocks {
     //  レジストリの作成
     public static final DeferredRegister<Block> BLOCKS=
@@ -18,8 +23,15 @@ public class SaoModBlocks {
     //  レジストリにブロックを登録
     //  ダイヤモンドブロックの設定をコピー
     //  金床の設置音を設定
-    public static final RegistryObject<Block>ORIHALCON_BLOCK = BLOCKS.register("orihalcon_block",
+    public static final RegistryObject<Block>ORIHALCON_BLOCK =registerBlockItem("orihalcon_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK).sound(SoundType.ANVIL)));
+
+    private static <T extends Block>RegistryObject<T>registerBlockItem(String name, Supplier<T> supplier){
+        RegistryObject<T>block=BLOCKS.register(name,supplier);
+        SaoModItems.ITEMS.register(name,
+                () -> new BlockItem(block.get(),new Item.Properties()));
+        return block;
+    }
 
     //  レジストリをイベントバスに登録する
     public static void reister(IEventBus eventBus){
